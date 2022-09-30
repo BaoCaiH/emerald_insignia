94a4dc5ecefe9a7078390e20385b5e44babbf6e7
BaoCaiH <caihongbao280996@gmail.com>
2022-09-30 23:13:10 +0200

    setup game

---
+++ src/elements/Board.ts
@@ -0,0 +1,11 @@
+import { Sprite, settings, SCALE_MODES } from "pixi.js";
+
+// Pixel rendering settings
+settings.SCALE_MODE = SCALE_MODES.NEAREST;
+
+const board: Sprite = Sprite.from("img/Map/00.png");
+board.anchor.set(0);
+board.x = 0;
+board.y = -7;
+
+export default board;
