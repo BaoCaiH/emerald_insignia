5cec2b6c28f1705baef93a5a4b71c3eb50a95de6
BaoCaiH <caihongbao280996@gmail.com>
2022-10-06 02:02:13 +0200

    add collision to board

--- src/elements/Board.ts
+++ src/elements/Board.ts
@@ -1,12 +1,36 @@
 import { Sprite, Texture } from "pixi.js";
+import { maps } from "./maps";
+import { toGridCoordString } from "../utils/coordinates";
+
+// console.log(fs.readFileSync);
 
 class Board extends Sprite {
+  collisions: { [x: string]: boolean };
   constructor(config: { mapPath: string; xOffset?: number; yOffset?: number }) {
     super();
     this.texture = Texture.from(config.mapPath);
     this.anchor.set(0);
     this.x = config.xOffset || 0;
     this.y = config.yOffset || 0;
+    this.collisions = this.loadCollision(config.mapPath);
+    console.log(this.collisions);
+  }
+
+  loadCollision(mapPath: string) {
+    const collisionPath = `${mapPath.split(".")[0].split("/").at(-1)}`;
+    return maps[collisionPath]["floorCollision"]
+      .map(([x, y]) => {
+        return { [toGridCoordString(x, y)]: true };
+      })
+      .reduce((prev, curr) => {
+        const [key, value] = Object.entries(curr)[0];
+        prev[key] = value;
+        return prev;
+      }, {});
+  }
+
+  isOccupied(x: number, y: number) {
+    return this.collisions[`${x},${y}`] || false;
   }
 }
 
