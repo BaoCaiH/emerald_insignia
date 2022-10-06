69ee3c058b6efb0f7337c95fa8697140bf2613d0
BaoCaiH <caihongbao280996@gmail.com>
2022-10-06 02:01:51 +0200

    grid utility functions

---
+++ src/utils/coordinates.ts
@@ -0,0 +1,28 @@
+export const toGridCoord = (x: number, y: number, tileSize: number = 16) => {
+  return { x: x * tileSize, y: y * tileSize };
+};
+
+export const fromGridCoord = (x: number, y: number, tileSize: number = 16) => {
+  return { x: x / tileSize, y: y / tileSize };
+};
+
+export const toGridCoordString = (
+  x: number,
+  y: number,
+  tileSize: number = 16
+) => {
+  return `${x * tileSize},${y * tileSize}`;
+};
+
+export const nextPosition = (
+  x: number,
+  y: number,
+  direction: string,
+  tileSize: number = 16
+) => {
+  if (direction === "left") return { x: x - tileSize, y: y };
+  else if (direction === "right") return { x: x + tileSize, y: y };
+  else if (direction === "up") return { x: x, y: y - tileSize };
+  else if (direction === "down") return { x: x, y: y + tileSize };
+  else return { x: x, y: y };
+};
