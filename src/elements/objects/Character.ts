d469b2c60d107495a587751093175be4d79a6864
BaoCaiH <caihongbao280996@gmail.com>
2022-10-09 00:19:11 +0200

    enable variety of movespeed

--- src/elements/objects/Character.ts
+++ src/elements/objects/Character.ts
@@ -22,10 +22,10 @@ class Character extends GameObject {
     this.moveSpeed = config.moveSpeed || 1;
     this.tweenRemaining = 0;
     this.tweeningInstruction = {
-      left: ["x", -this.moveSpeed],
-      right: ["x", this.moveSpeed],
-      up: ["y", -this.moveSpeed],
-      down: ["y", this.moveSpeed],
+      left: ["x", -1],
+      right: ["x", 1],
+      up: ["y", -1],
+      down: ["y", 1],
     };
   }
 
@@ -41,7 +41,7 @@ class Character extends GameObject {
     }
   }
 
-  private startTweening(arrow: string) {
+  protected startTweening(arrow: string) {
     if (this.direction !== arrow) {
       this.changeDirection(arrow);
     }
@@ -51,26 +51,27 @@ class Character extends GameObject {
     }
   }
 
-  private changeDirection(arrow: string) {
+  protected changeDirection(arrow: string) {
     if (this.direction !== arrow) {
       this.direction = arrow;
       this.changeAnimation(arrow);
     }
   }
 
-  private tweening() {
+  protected tweening() {
     if (this.tweenRemaining > 0 && this.direction !== "idle") {
       const [axis, instruction] = this.tweeningInstruction[this.direction];
+      const movement = Math.min(this.moveSpeed, this.tweenRemaining);
       if (axis === "x") {
-        this.internalSprite.x += instruction;
+        this.internalSprite.x += instruction * movement;
       } else if (axis === "y") {
-        this.internalSprite.y += instruction;
+        this.internalSprite.y += instruction * movement;
       }
-      this.tweenRemaining -= this.moveSpeed;
+      this.tweenRemaining -= movement;
     }
   }
 
-  private attemptMoveSuccess(direction: string) {
+  protected attemptMoveSuccess(direction: string) {
     const { x, y } = nextPosition(this.x, this.y, direction);
     return !this.board.isOccupied(x, y);
   }
